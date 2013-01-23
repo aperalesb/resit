c5d302bb6c1c53657e299f1b49f0ed81e3bb8bb6
Amin <heavywizard@gmail.com>
2013-01-23 21:38:48 +0100

    Securizado el login de la aplicación web. Falta dar de alta en sesión el usuario autenticado

--- resit-controller/src/main/java/com/resit/security/ResitAuthenticationProvider.java
+++ resit-controller/src/main/java/com/resit/security/ResitAuthenticationProvider.java
@@ -1,22 +1,38 @@
 package com.resit.security;
 
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
+
+import com.resit.controller.UserController;
 
 public class ResitAuthenticationProvider implements AuthenticationProvider {
 
-	@Override
-	public Authentication authenticate(Authentication arg0)
-			throws AuthenticationException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	/**
+     * Log.
+     */
+	private final static Logger LOG = Logger.getLogger(ResitAuthenticationProvider.class);
 
-	@Override
-	public boolean supports(Class<?> arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    private static final String ROLE_LOGGED = "ROLE_LOGGED";
+
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        Authentication authent = null;
+        return authent;
+    }
+
+    public boolean supports(Class authentication) {
+        return true;
+    }
 
 }
